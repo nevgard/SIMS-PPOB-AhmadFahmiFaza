073e12686433f5ccddb5f634901e889aa564d678
nevgard <fahmifaza6@gmail.com>
2024-12-06 20:21:00 +0700

    fix : type js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
